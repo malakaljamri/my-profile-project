55437dfcfab1ad22999e34d81c478c3d08ffdb8e
malakaljamri <malakaljamri02@gmail.com>
2025-03-13 21:36:30 +0300

    m

--- my-profile-project/src/App.jsx
+++ my-profile-project/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./login";  // Import Login component
 import Dashboard from "./Dashboard";  // Import Dashboard component
 import NotFound from "./NotFound"; // The 404 page
